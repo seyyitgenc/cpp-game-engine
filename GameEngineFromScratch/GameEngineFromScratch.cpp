6b624c709bd89e5f4256f7b72e22d9af5a2477e0
Seyyit Genç <mahluk053@gmail.com>
2022-10-26 20:06:30 +0300

    Add project files.

---
+++ GameEngineFromScratch/GameEngineFromScratch.cpp
@@ -0,0 +1,10 @@
+// GameEngineFromScratch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "test" << std::endl;
+}
