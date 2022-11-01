f43c6bb46896790302eba4b2f876888c4bdb5d5e
Seyyit Genç <mahluk053@gmail.com>
2022-11-01 16:49:09 +0300

    changed to SDL2

---
+++ GameEngineFromScratch/game.cpp
@@ -0,0 +1,77 @@
+#pragma once
+#include "game.h"
+
+Game::Game()
+{
+}
+Game::~Game()
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+void Game::Init(const char* title, int x, int y, int w, int h, int flags) {
+	this->flags = flags;
+	if (fullscreen)
+		this->flags = flags | SDL_WINDOW_FULLSCREEN;
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+		std::cout << "Subsystems Initialized";
+		window = SDL_CreateWindow(title, x, y, w, h, flags);
+		if (window) {
+			std::cout << "Window Created!\n";
+			SDL_SetWindowMinimumSize(window, 100, 100);
+		}
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer) {
+			SDL_SetRenderDrawColor(renderer, 155, 240, 140, 150);
+			std::cout << "Renderer Created!\n";
+			SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+			isOpen = true;
+		}
+	}
+}
+
+void Game::handleEvents()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT)
+			this->isOpen = false;
+		else {
+			if (event.type == SDL_KEYDOWN)
+				switch (event.key.keysym.sym) {
+				case SDLK_w:
+					std::cout << "wDOWN" << std::endl;
+					break;
+				case SDLK_s:
+					std::cout << "sDOWN" << std::endl;
+				}
+			else if (event.type == SDL_KEYUP) {
+				switch (event.key.keysym.sym) {
+				case SDLK_w:
+					std::cout << "wUP" << std::endl;
+					break;
+				case SDLK_s:
+					std::cout << "sUP" << std::endl;
+				}
+			}
+		}
+	}
+}
+
+void Game::render()
+{
+	SDL_SetRenderDrawColor(this->renderer, 140, 140, 140, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_RenderPresent(renderer);
+}
+
+void Game::update()
+{
+}
+
+void Game::setFullScreen()
+{
+	this->flags = this->flags | SDL_WINDOW_FULLSCREEN;
+}
