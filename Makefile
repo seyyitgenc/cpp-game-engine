b3cfcbaba637ce5583e923110ebb4790c5a69cd8
Seyyit Genç <mahluk053@gmail.com>
2022-11-02 16:52:52 +0300

    test .gitignore
    test .gitignore

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 SRC_DIR = src
-BUILD_DIR = build
+BUILD_DIR = build/debug
 CC = g++
 SRC_FILES = $(wildcard $(SRC_DIR)/*.cpp)
 OBJ_NAME = play
-INCLUDE_PATHS = -Iinclude
-LIBRARY_PATHS = -Llib
+INCLUDE_PATHS = -I include
+LIBRARY_PATHS = -L lib
 
 all:
-	$(CC)  $(INCLUDE_PATHS) $(LIBRARY_PATHS) -o play $(SRC_FILES) -lmingw32 -lSDL2 -lSDL2main
+	$(CC)  $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(SRC_FILES) -o $(BUILD_DIR)/$(OBJ_NAME) -lmingw32 -lSDL2 -lSDL2main
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include "game.h"
 #undef main
 
-Game* game = nullptr;
+Game *game = nullptr;
 
-int main() {
+int main()
+{
 	game = new Game();
 	game->Init("test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_RESIZABLE);
-	game->setRenderColor(0,0,0,0);
-	while (game->isRunnig()) {
+	game->setRenderColor(0, 0, 0, 0);
+	int w = 0, s = 0;
+	while (game->isRunnig())
+	{
 		game->handleEvents();
 		game->render();
 		game->update();
+		const std::uint8_t *keystates = SDL_GetKeyboardState(nullptr);
+		if (keystates[SDL_SCANCODE_W])
+		{
+			w++;
+			std::cout << w << std::endl;
+		}
+		if (keystates[SDL_SCANCODE_S])
+		{
+			s++;
+			std::cout << s << std::endl;
+		}
 	}
 	game->~Game();
 	return 0;
 }
-//simple update function
-void update() {
-	//if things could update the code would go in here.
+// simple update function
+void update()
+{
+	// if things could update the code would go in here.
 }
