1572d7a4042c13321a421a980ceca99f8473ed25
Seyyit Genç <mahluk053@gmail.com>
2023-07-29 12:40:40 +0300

    cmake fixes

--- Source/Engine/globals.h
+++ Source/Engine/globals.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <SDL.h>
-#include <SDL_mixer.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
 #include <iostream>
 
--- Source/Engine/texture.h
+++ Source/Engine/texture.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <SDL_image.h>
+#include <SDL2/SDL_image.h>
 
 #include <string>
 
