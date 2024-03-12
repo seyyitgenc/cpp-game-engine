605922d5d9829ff4d096f4b9a0a913d1354fbcb7
Seyyit Genc <seyyitgenc08@gmail.com>
2024-03-12 14:03:19 +0300

    fix: if shadering reloading failed. keep using last one

--- src/engine/shader.h
+++ src/engine/shader.h
@@ -10,13 +10,28 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_glfw.h"
+#include "ImGui/imgui_impl_opengl3.h"
 
 struct ShaderInfo{
     std::string vertexPath;
     std::string fragmentPath;
     std::string geometryPath;
     std::string description;
-    GLuint ID;
+    GLuint ID = -1;
+    void drawInfo(){
+        if (ImGui::BeginTabItem("Details")){
+            ImGui::Text("Shader ID : %d", ID);
+            ImGui::Text("Shader Vertex Path   : %s", vertexPath.c_str());
+            ImGui::Text("Shader Fragment Path : %s", fragmentPath.c_str());
+            ImGui::Text("Shader Geometry Path : %s", geometryPath.c_str());
+            static float test[3] = {0,1,1};
+            ImGui::SliderFloat3("testing",test,0,100);
+            ImGui::TextWrapped("Description : %s", description.c_str());
+            ImGui::EndTabItem();
+        }
+    }
 };
 
 class Shader
@@ -25,26 +40,8 @@ public:
     Shader(const std::string &vertexPath, const std::string &fragmentPath);
     Shader(const std::string &vertexPath, const std::string &geometrypath, const std::string &fragmentPath);
     ShaderInfo &getShaderInfo(){return _info;}
-    // template<typename T>
-    // T getAttribute(const std::string &name){
-    //     return static_cast<T>(getGLSLType<T>(name));
-    // }
-private:
-    void initAttributeList(){
-        GLint numUniforms;
-        glGetProgramiv(_info.ID, GL_ACTIVE_UNIFORMS, &numUniforms);
-        for(GLint i = 0; i < numUniforms; i++)
-        {
-            char buffer[128];
-            GLsizei length = 0;
-            GLint size = 0;
-            GLenum type = 0;
-            glGetActiveUniform(_info.ID, i, sizeof(buffer), &length, &size, &type, buffer);
-            attributeList.push_back(std::make_pair(buffer,type));
-        }   
-    }
+
 public:  // uniforrm setter functions
-    std::vector<std::pair<std::string, GLenum>> attributeList;
     void setBool(const std::string &name, bool value) const;
    
     void setInt(const std::string &name, int value) const;
@@ -55,6 +52,7 @@ public:  // uniforrm setter functions
     void setVec4(const std::string &name, glm::vec4 value) const;
 
     void setMat4(const std::string &name, glm::mat4 value) const;
+
 public: // uniform getter functions
     bool getBool(const std::string &name);
     
@@ -70,34 +68,9 @@ public:
     //  
     void reload();
     void use();
-private:
-    template<typename T>
-    std::string getGLSLType(GLenum type){
-        switch(type) {
-            case GL_FLOAT_VEC2: return "vec2";
-            case GL_FLOAT_VEC3: return "vec3";
-            case GL_FLOAT_VEC4: return "vec4";
-            case GL_FLOAT:      return "float";
-            case GL_BOOL:       return "bool";
-            case GL_INT:        return "int";
-            // case GL_INT_VEC2: return "ivec2";
-            // case GL_INT_VEC3: return "ivec3";
-            // case GL_INT_VEC4: return "ivec4";
-            // case GL_BOOL_VEC2: return "bvec2";
-            // case GL_BOOL_VEC3: return "bvec3";
-            // case GL_BOOL_VEC4: return "bvec4";
-            // case GL_FLOAT_MAT2: return "mat2";
-            // case GL_FLOAT_MAT3: return "mat3";
-            // case GL_FLOAT_MAT4: return "mat4";
-            // case GL_SAMPLER_2D: return "sampler2D";
-            // case GL_SAMPLER_CUBE: return "samplerCube";
-            // add more types as needed
-            default: return "unknown";
-        }
-    }
     std::string readFile(const std::string &path);
-    void compileShader(const char*  code, GLenum type);
-    void buildShader();
+    bool compileShader(const char*  code, GLuint ID, GLenum type);
+    bool buildShader();
 private:
     // TODO: add descripton
     // std::string _description;
